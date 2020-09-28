4bac7667386993a4fb6edd5f6243ba337f4cfefc
SydNs <sydneynsiimbe@gmail.com>
2020-09-28 14:01:03 +0300

    removed the secret key

--- yaridDRF/settings.py
+++ yaridDRF/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3!tnape^!_#fdgd&6jy2435u)#)c+&%22*em%y2+l##)g8b9jv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
